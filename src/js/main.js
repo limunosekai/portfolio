8a1da949a71e5439ba83d5665f9f570fadf880d7
limunosekai <powerlsh0103@gmail.com>
2021-03-17 01:59:56 +0900

    Page2 추가

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,15 @@
+/**
+ * Page1--------------------------------------------------------------
+ */
+
 const bar = document.querySelector('.progressBar');
 const cover = document.querySelector('.cover');
-const rock1 = document.querySelector('.img_rock1');
-const rock2 = document.querySelector('.img_rock2');
-const leaf3 = document.querySelector('.img_leaf3');
-const leaf4 = document.querySelector('.img_leaf4');
-const leftLeaf = document.querySelector('.img_left_leaf');
-const rightLeaf = document.querySelector('.img_right_leaf');
+const rock1 = document.querySelector('.p1_img_rock1');
+const rock2 = document.querySelector('.p1_img_rock2');
+const leaf3 = document.querySelector('.p1_img_leaf3');
+const leaf4 = document.querySelector('.p1_img_leaf4');
+const leftLeaf = document.querySelector('.p1_img_left_leaf');
+const rightLeaf = document.querySelector('.p1_img_right_leaf');
 
 // 스크롤 top 좌표
 let scrollTop = 0;
@@ -18,10 +22,12 @@ let my = 0;
 // 이미지 이동 속도
 let speed = 0.0002;
 
+// 초기화
 window.onload = () => {
   cover.style.opacity = 0;
 };
 
+// progress bar
 const progressHandler = (e) => {
   scrollTop = document.documentElement.scrollTop;
 
@@ -29,11 +35,18 @@ const progressHandler = (e) => {
     (scrollTop / (document.body.scrollHeight - window.innerHeight)) * 100
   );
   bar.style.height = per + '%';
-  cover.style.opacity = 0.1 + scrollTop / 850;
-  rock1.style.transform = `translate(${-scrollTop / 10}px,0)`;
-  rock2.style.transform = `translate(${scrollTop / 10}px,0)`;
 };
 
+// Page1 scroll evnet
+const pageOneMovingHandler = (e) => {
+  scrollTop = document.documentElement.scrollTop;
+
+  cover.style.opacity = 0.1 + scrollTop / 850;
+  rock1.style.transform = `translate(${-scrollTop / 5}px,0)`;
+  rock2.style.transform = `translate(${scrollTop / 5}px,0)`;
+};
+
+// Page1 mousemove event
 const moveHandler = (e) => {
   x = e.clientX - window.innerWidth / 2;
   y = e.clientY - window.innerHeight / 2;
@@ -41,16 +54,61 @@ const moveHandler = (e) => {
   loop();
 };
 
+// Page1 loop animation
 const loop = () => {
   mx += (x - mx) * speed;
   my += (y - my) * speed;
 
-  leaf3.style.transform = `rotate(${-(mx / 30) - -(my / 30)}deg)`;
+  leaf3.style.transform = `rotate(${-(mx / 20) - -(my / 20)}deg)`;
   leaf4.style.transform = `rotate(${-(mx / 50) - -(my / 50)}deg)`;
   leftLeaf.style.transform = `scale(${1 + mx / 14000 + my / 14000})`;
-  rightLeaf.style.transform = `scale(${1 + mx / 14000 + my / 14000})`;
+  rightLeaf.style.transform = `scale(${1 + mx / 10000 + my / 10000})`;
   window.requestAnimationFrame(loop);
 };
 
 window.addEventListener('mousemove', moveHandler, false);
 window.addEventListener('scroll', progressHandler, false);
+window.addEventListener('scroll', pageOneMovingHandler, false);
+
+/**
+ * Page2--------------------------------------------------------------
+ */
+const sectionTwo = document.querySelector('.page2');
+const profile = document.querySelector('.p2_img_profile');
+const text = document.querySelector('.p2_profile_text');
+
+// Page2 scroll event
+
+function debounce(func, wait = 20, immediate = true) {
+  var timeout;
+  return function () {
+    var context = this,
+      args = arguments;
+    var later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+
+// image slide
+const checkSlide = (e) => {
+  // image
+  const slideInAt = window.scrollY + window.innerHeight - profile.height / 4;
+  const imageBottom = profile.offsetTop + profile.height;
+  const isHalfShown = slideInAt > profile.offsetTop;
+  const isNotScrolledPast = window.scrollY < imageBottom;
+  if (isHalfShown && isNotScrolledPast) {
+    profile.classList.add('active');
+    text.classList.add('active');
+  } else {
+    profile.classList.remove('active');
+    text.classList.remove('active');
+  }
+};
+
+window.addEventListener('scroll', debounce(checkSlide), false);
